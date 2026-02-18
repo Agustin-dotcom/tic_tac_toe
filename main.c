cba18ee0bcbbe6f4537bcca93bb7c23002010e15
Agustin-dotcom <Agustin.Prieto2@alu.uclm.es>
2026-02-18 13:18:24 +0100

    Added skeleton to print board which is represented by a matrix 3x3

---
+++ main.c
@@ -0,0 +1,31 @@
+Â#include<stdi.h>
+
+void display_matrix(int matrix[n][n], int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(matrix[i][j]==0){
+				printf("  ");
+			}
+			if(matrix[i][j]==1){
+				printf("x ");
+			}
+		}
+		printf("\n");
+	}
+}
+/*
+(1) Displays the matrix which is the board, grid of the tic tac toe
+(2) Initializes all values to 0 which means nobody put a circle or a cross
+    in that cell
+*/
+void main(){
+	int n = 3;
+	int matrix[n][n] = new int[n][n];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			matrix[i][j] = 0; // (2)
+		}
+	}
+	display_matrix(matrix,n); // (1)
+}
+
